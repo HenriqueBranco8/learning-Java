6c0bf371668d182facd7da047f9e2187ee298105
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-07-13 17:18:43 -0300

    Aprendendo sobre OperadoresAritimeticos e tipos de dados em java

---
+++ Aulas/Operadores_Aritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,21 @@
+
+static void main(String[] args) {
+//    private static void prePos(){
+//        int k = 10;
+//
+//        int i = ++k;
+//        int j = k++;
+//        int x = k;
+//
+//        System.out.println("k: " + k);
+//        System.out.println("i (pré-fixado): " + i);
+//        System.out.println("j (pós-fixado): " + j);
+//        System.out.println("x: " + x);
+
+//    }
+
+        int a;
+        float b = 3.5f;
+
+}
+
---
+++ Aulas/Tipos_de_Dados/tipoDados.java
@@ -0,0 +1,51 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    byte b1 = 10;
+    byte b2 = 1;
+
+    short s1 = 20000;
+//    short s2 = 40000;
+
+//    int i1 = -10000000000;
+    int i2 = 28500;
+
+    long l1 = 1000000000000000000L;
+    long l2 = 1000000000000020002L;
+
+
+//    float f1 = 4.5;
+    float f2 = 10.68F;
+
+    double d1 = 85.36D;
+    double d2 = 99.840;
+
+    char c1 = 'w';
+//    char c2 = 'Tw';
+    char c3 = '\u0057';
+
+    String st1 = "Texto";
+    String st2 = "Text2";
+    String st3 = "Texto longo e um pouco longo";
+
+    boolean bo1 = true;
+    boolean bo2 = false;
+
+    System.out.println(b1);
+    System.out.println(b2);
+    System.out.println(s1);
+    System.out.println(i2);
+    System.out.println(l1);
+    System.out.println(l2);
+    System.out.println(f2);
+    System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(c1);
+    System.out.println(c3);
+    System.out.println(st1);
+    System.out.println(st2);
+    System.out.println(st3);
+    System.out.println(bo1);
+    System.out.println(bo2);
+}
