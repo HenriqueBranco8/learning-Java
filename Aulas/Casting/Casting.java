a64c91b00d5627ce84da5655c1dceaeb5c06889b
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-07-13 18:06:33 -0300

    Aprendendo sobre casting em java.

---
+++ Aulas/Casting/Casting.java
@@ -0,0 +1,20 @@
+
+
+static void main(String[] args) {
+    byte pouco;
+
+    short menor = 10;
+
+    int bom;
+
+    long grande = 95211256456544546L;
+
+    float flutuante = 2F;
+    double grandeComPonto = 1D;
+
+
+    pouco = (byte) grandeComPonto;
+    System.out.println(pouco);
+}
+
+
